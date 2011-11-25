3ffc7052b2cb516e01cb33abf5855a9a954e8be2
Nick <nick@Macbook.local>
2011-11-24 23:18:00 -0600

    first

---
+++ pull.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require "awesome_print"
+require 'net/http'
+require 'yesradio'
+require 'mongo'
+require 'time'
+file = File.new("stations.txt", "r")
+
+
+@con = Mongo::Connection.new
+@db = @con['holiday']
+@songs = @db['songs']
+
+file.readlines.each do |station|
+  @list = Yesradio::get_log :name => station.chomp, :ago => 1
+  ap station.chomp
+  @list.each do |song|
+    entry = {:station => station.chomp, 
+            :title => song.title, 
+            :at => Time.parse(song.at.to_s).utc, 
+            :by => song.by,
+            :cover => song.cover,
+            :song_yes_id => song.id
+            }
+    
+    @songs.save(entry)
+  end
+end
+
+
